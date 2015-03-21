27c302a2e528623b1e2a215996d0ad2d7c6c089f
robin <863579016@qq.com>
2015-03-21 09:04:49 +0800

    spring boot hello world

---
+++ src/main/java/hello/world/SimpleController.java
@@ -0,0 +1,28 @@
+package hello.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by root on 15-3-21.
+ */
+@Controller
+@EnableAutoConfiguration
+public class SimpleController {
+
+    @RequestMapping(value ="/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(){
+        return "hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleController.class, args);
+    }
+
+
+}
